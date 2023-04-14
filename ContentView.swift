82eef3bc49990bbffa2c07a3c2a5ea58a839884f
Teodor Djuric <tdjuric5081@stu.d214.org>
2023-04-14 12:57:58 -0500

    Hello

--- ContentView.swift
+++ ContentView.swift
@@ -1,12 +1,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var number1: Int = 0
+    @State var number2: Int = 0
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            TextField("Enter Integer", value: $number1, format: .number)
+            TextField("Enter Integer", value: $number2, format: .number)
+
         }
     }
 }
