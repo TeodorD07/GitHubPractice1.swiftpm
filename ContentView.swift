83a68d280331e703aa9fbfdeef7eab2f1bb69686
Teodor Djuric <tdjuric5081@stu.d214.org>
2023-04-14 13:27:32 -0500

    Modifiers

--- ContentView.swift
+++ ContentView.swift
@@ -7,22 +7,52 @@ struct ContentView: View {
     var body: some View {
         VStack {
             TextField("Enter Integer", value: $number1, format: .number)
+                .textFieldStyle(.roundedBorder)
+                .keyboardType(.numberPad)
+                .multilineTextAlignment(.center)
+                .padding()
+            
             TextField("Enter Integer", value: $number2, format: .number)
+                .textFieldStyle(.roundedBorder)
+                .keyboardType(.numberPad)
+                .multilineTextAlignment(.center)
+                .padding()
+            
             Button("Multiply"){
                 answer = number1 * number2
             }
+            .foregroundColor(.red)
+            .font(.largeTitle)
+            .frame(width: 100, height: 50)
+            .background(.black)
+            
             Button("Divide"){
                 
             }
+            .foregroundColor(.red)
+            .font(.largeTitle)
+            .frame(width: 100, height: 50)
+            .background(.black)
+            
             Button("Add"){
                 
             }
+            .foregroundColor(.red)
+            .font(.largeTitle)
+            .frame(width: 100, height: 50)
+            .background(.black)
+            
             Button("Subtract"){
                 
             }
+            .foregroundColor(.red)
+            .font(.largeTitle)
+            .frame(width: 100, height: 50)
+            .background(.black)
             
             Text ("\(answer)")
-            
+                .bold()
+                .padding()
         }
     }
 }
