a41f7e63f55414805abbe42907c25afd05256883
Teodor Djuric <tdjuric5081@stu.d214.org>
2023-04-14 13:15:09 -0500

    I added a button

--- ContentView.swift
+++ ContentView.swift
@@ -2,8 +2,12 @@ import SwiftUI
 
 struct ContentView: View {
     @State var answer = 0
+    @State var number1 = 0
+    @State var number2 = 0
     var body: some View {
         VStack {
+            TextField("Enter Integer", value: $number1, format: .number)
+            TextField("Enter Integer", value: $number2, format: .number)
             Button("Multiply"){
                 
             }
